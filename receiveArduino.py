cf049c51298a563cf29eacd4569a511c850c188c
AsariPi <aslan.sari022@gmail.com>
2016-10-16 04:36:54 +0300

    add receive.py code for pi

---
+++ receiveArduino.py
@@ -0,0 +1,41 @@
+import Rpi.GPIO as GPIO
+from lib_nrf24 import NRF24
+import time
+import spidev
+
+GPIO.setmode(GPIO.BCM)
+
+pipes=[[0xE8, 0xE8, 0xF0, 0xF0,0xE1],[0xF0, 0xF0, 0xF0, 0xF0, 0xE1]]
+
+radio = NRF24(GPIO, spidev.SpiDev())
+radio.begin(0,17)
+
+radio.setPayloadSize(32)
+radio.setChannel(0x76)
+radio.setDataRate(NRF.BR_1MBPS)
+radio.setPALevel(NRF.PA_MIN)
+
+radio.setAutoAck(True)
+radio.enableDynamicPayloads()
+radio.enableAckPayload()
+
+radio.openReadingPipe(1,pipes[1])
+radio.printDetails()
+radio.startListening()
+
+while True:
+
+    while not radio.available(0):
+        time.sleep(1/100)
+
+    receivedMessage=[]
+    radio.read(receivedMessage, radio.getDynamicPayloadSize())
+    print("Received:{}".format(receivedMessage))
+
+    print("Translating our received message into unicode characters..")
+    string=""
+
+    for n in receivedMessage:
+        if(n >= 32 and n <= 126):
+            string +=chr(n)
+    print("Our received message decodes to: {}".format(string))
