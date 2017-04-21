0556ff9956a9ef053c1a752aab272c32ae719185
aslansari <aslansari>
2017-04-21 10:15:13 +0300

    Added Plug module

--- node4000/node4000.ino
+++ node4000/node4000.ino
@@ -1,22 +1,25 @@
 #include <SPI.h>
 #include <RF24.h>
-#include <IRremote.h>
 
-#define RECV_PIN 5
-
-#define parent 0xE8E8F0F0E1LL
-#define childA 0xF0F0A040E1LL
-#define childB 0xF0F0B040E1LL
-#define deviceaddr "4000"
 //ce , csn pins
 RF24 radio(9,10);
 
-IRrecv irrecv(RECV_PIN);
-
-decode_results results;
-
+#define parent 0xF0F0F0F0E1LL
+#define childA 0xF0F0A030E1LL
+#define childB 0xF0F0B030E1LL
+#define deviceaddr "4000"
+/*    This module is one of the child of raspberryPi.
+ *     1000 represents that the module is in the first layer of nodes
+ *     each digit shows where module works on network
+ *     for ex. first digit says it is the first module in the first layer
+ *     if its '1B00' that module would be the second child module of 1000 
+*/
+const int PLUG=3;
+boolean writeFlag;
 int i=0;
 int counter=0;
+unsigned long time;
+
 char receivedMessage[32]={0};
 char transmitMessage[32]={0};
 char sender_address[5]={0};
@@ -24,11 +27,7 @@ char address[5]={0};
 char command[17]={0};
 char mdata[9]={0};
 
-String str_data = "";
-String str_cmd = "";
-String str_address = "";
-
-void setup() {
+void setup(){
   while(!Serial);
   Serial.begin(9600);
   
@@ -36,90 +35,104 @@ void setup() {
   radio.setPALevel(RF24_PA_MAX);
   radio.setChannel(0x76);
   radio.openWritingPipe(parent);
-  const uint64_t pipe = 0xF0F0A030E1LL;
+  const uint64_t pipe = 0xE8E8F0F0E1LL;
   radio.openReadingPipe(1,pipe);
     
   radio.enableDynamicPayloads();
   radio.powerUp();
 
-  irrecv.enableIRIn();  //start the receiver
+  pinMode(PLUG,OUTPUT);
+
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  if(irrecv.decode(&results)){
-  	int button = translateIR();
+void loop(){
+  radio.startListening();
+  Serial.println("Starting Loop. Radio on.");
 
-  	switch(button){
-  		case 201: 
-  			str_address = "3000";
-  			str_cmd = "LIGHT";
-  			str_data = "ON";
-  			break;
-  		case 203:
-  			str_address = "3000";
-  			str_cmd = "LIGHT";
-  			str_data = "OFF";
-  			break;
-  		default:
-  			str_address = "";
-  			str_cmd = "";
-  			str_data = "";
-  	}
-    String str_pi ="0000";
-  	str_pi.toCharArray(address,sizeof(str_pi));
-  	str_address.toCharArray(sender_address,sizeof(str_address));
-  	str_cmd.toCharArray(command,sizeof(str_cmd));
-  	str_data.toCharArray(mdata,sizeof(mdata));
-  	writeFrame();
+  if(radio.available()){
+    radio.read(receivedMessage, sizeof(receivedMessage));
+    Serial.println(receivedMessage);
+    Serial.println("Turning off the radio.");
+    radio.stopListening();
+    
+    readFrame();  //read received Frame and assign in dedicated array
+    String str_address = String(address);
+    if(str_address=="4000"){
+      Serial.println("you are in the right node");
+    }
+    else if(str_address=="0000"){
+      Serial.println("message is sending to raspverri pi");
+      radio.openWritingPipe(parent);
+      radio.write(receivedMessage,sizeof(receivedMessage));
+    }
+    else{
+      for(int i=0;i<=3;i++){
+        Serial.println(address[i]);
+      }
+      switch (address[1]){
+        case 'A':
+        Serial.println("childA data sending..");
+        radio.stopListening();
+        radio.openWritingPipe(childA);
+        radio.write(receivedMessage,sizeof(receivedMessage));
+        Serial.println("childA data has sent..");
+        break;
+        case 'B':
+        radio.openWritingPipe(childB);
+        radio.write(receivedMessage,sizeof(receivedMessage));
+        break;
+        default:
+        Serial.println("address not found");
+        break;
+      }
+    }
+    
+    String str_data ="";
+    str_data = String(mdata);
+    String str_command = String(command); //transformation for if statement
+    Serial.println(str_command);
+    
+    if (str_command == "PLUG"){
+      Serial.println("str_data="+str_data);
+      if(str_data == "ON"){
+        Serial.println("turning lights on...");
+        digitalWrite(PLUG,LOW);
+        String message = "ACK";
+        message.toCharArray(mdata,sizeof(message));
+        writeFrame();
 
-  	radio.openWritingPipe(parent);
-  	radio.write(transmitMessage,sizeof(transmitMessage));
-    Serial.println(transmitMessage);
+        radio.openWritingPipe(parent); //open pipe 
+        radio.write(transmitMessage,sizeof(transmitMessage));
+      }
+      else if(str_data == "OFF"){
+        Serial.println("Turning lights off...");
+        digitalWrite(PLUG,HIGH);
+        String message = "ACK";
+        message.toCharArray(mdata,sizeof(message));
+        writeFrame();
 
-    irrecv.resume();  //receive the next value
+        radio.openWritingPipe(parent); //open pipe 
+        radio.write(transmitMessage,sizeof(transmitMessage));
+      }
+      else{
+        Serial.println("Data couldn't read");
+        String message = "NACK";
+        message.toCharArray(mdata,sizeof(message));
+        writeFrame();
+
+        radio.openWritingPipe(parent); //open pipe 
+        radio.write(transmitMessage,sizeof(transmitMessage));
+      }
+    }
   }
 }
 
-int translateIR() // takes action based on IR code received
-// describing Remote IR codes 
-{
-	int basilan_buton=0;
-  switch(results.value)
-  {
-    case 0xFF629D: Serial.println(" FORWARD"); basilan_buton=101; break; //basılan buton değişkenine atanan değerin yüzler basamağı hangi satırda 
-    case 0xFF22DD: Serial.println(" LEFT");    basilan_buton=201; break; //diğer iki rakam da kaçıncı buton olduğunu gösteriyor
-    case 0xFF02FD: Serial.println(" -OK-");    basilan_buton=202; break;
-    case 0xFFC23D: Serial.println(" RIGHT");   basilan_buton=203; break;
-    case 0xFFA857: Serial.println(" REVERSE"); basilan_buton=301; break;
-    case 0xFF6897: Serial.println(" 1");       basilan_buton=401; break;
-    case 0xFF9867: Serial.println(" 2");       basilan_buton=402; break;
-    case 0xFFB04F: Serial.println(" 3");       basilan_buton=403; break;
-    case 0xFF30CF: Serial.println(" 4");       basilan_buton=501; break;
-    case 0xFF18E7: Serial.println(" 5");       basilan_buton=502; break;
-    case 0xFF7A85: Serial.println(" 6");       basilan_buton=503; break;
-    case 0xFF10EF: Serial.println(" 7");       basilan_buton=601; break;
-    case 0xFF38C7: Serial.println(" 8");       basilan_buton=602; break;
-    case 0xFF5AA5: Serial.println(" 9");       basilan_buton=603; break;
-    case 0xFF42BD: Serial.println(" *");       basilan_buton=701; break;
-    case 0xFF4AB5: Serial.println(" 0");       basilan_buton=702; break;
-    case 0xFF52AD: Serial.println(" #");       basilan_buton=703; break;
-    case 0xFFFFFFFF: Serial.println(" REPEAT");basilan_buton=0;    break;  
-    default: 
-    Serial.println(" other button   "); basilan_buton=0;
-    
-  }// End Case
-  
-  delay(500); // Do not get immediate repeat
-  return basilan_buton;
-} //END translateIR
-
 void writeFrame(){
+  
   i=0;
   counter=i;
   while(address[i-counter]!='\0'){
     transmitMessage[i]=address[i-counter];
-    Serial.println(transmitMessage[i]);
     i++;
   }
   transmitMessage[i]='`';
@@ -127,14 +140,12 @@ void writeFrame(){
   counter=i;
   while(sender_address[i-counter]!='\0'){
     transmitMessage[i]=sender_address[i-counter];
-    Serial.println(transmitMessage[i]);
     i++;
   }
   transmitMessage[i]='`';
   i++;
   counter=i;
   while(command[i-counter]!='\0'){
-    Serial.println(command[i-counter]);
     transmitMessage[i]=command[i-counter];
     i++;
   }
@@ -142,7 +153,6 @@ void writeFrame(){
   i++;
   counter=i;
   while(mdata[i-counter]!='\0'){
-    Serial.println(mdata[i-counter]);
     transmitMessage[i]=mdata[i-counter];
     i++;
   }
@@ -155,6 +165,9 @@ void writeFrame(){
 void readFrame(){
   for(int t=0;t<sizeof(mdata);t++){
     mdata[t]='\0';
+  }
+  for(int t=0;t<sizeof(command);t++){
+    command[t]='\0';
   }
   i=0;
   counter=i;
@@ -180,6 +193,5 @@ void readFrame(){
     mdata[i-counter]=receivedMessage[i];
     i++;
   }
-
 }
 
